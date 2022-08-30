1aaaa315e130d372caa03be45a1f2403a0e6d1be
Nikunj Rathod <nikunj.rathod@ditechtesting.com>
2022-08-30 07:54:37 -0300

    load activity and main activity, made new project

---
+++ GenshinImpactRecipes/app/src/main/java/com/example/genshinimpactrecipes/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.genshinimpactrecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val favFragment = FavFragment()
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(homeFragment)
+        bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_home -> replaceFragment(homeFragment)
+                R.id.ic_favourite -> replaceFragment(favFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
+    }
+}
